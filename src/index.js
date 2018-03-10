f155f1de535a274043bc5d22d18ca712c8e40ead
Zhasulan <zhasulan.kukebaev@atrinity>
2018-03-11 02:45:18 +0300

    tic tac toe example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './x0components/x0game.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
